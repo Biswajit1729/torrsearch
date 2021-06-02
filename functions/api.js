ca5b56c71629c32a5008aea56a6c90bc454fb647
Biswajit1729 <mandalbis1729@gmail.com>
2021-06-02 17:17:58 +0530

    1st commit

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,21 @@ const TorrentSearchApi = require('torrent-search-api');
 const app = express();
 const port = 3000;
 TorrentSearchApi.enableProvider('Torrent9');
+TorrentSearchApi.enableProvider('Torrentz2');
+TorrentSearchApi.enableProvider('1337x');
+TorrentSearchApi.enableProvider('ThePirateBay');
+TorrentSearchApi.enableProvider('KickassTorrents');
+TorrentSearchApi.enableProvider('Rarbg');
+TorrentSearchApi.enableProvider('TorrentProject');
+TorrentSearchApi.enableProvider('Yts');
+TorrentSearchApi.enableProvider('Limetorrents');
+TorrentSearchApi.enableProvider('Eztv');
+
+
 
 app.get('/', async (req, res)  => {
-    const torrents = await TorrentSearchApi.search(['Torrent9'], '1080', 'Movies', 20);
+    const torrents = await TorrentSearchApi.search(
+        ['Torrent9','Torrentz2','1337x','ThePirateBay','KickassTorrents','Rarbg','TorrentProject','Yts','Limetorrents','Eztv'], ['720','hindi'], 'Movies', 20);
     res.send(torrents)
 });
 
