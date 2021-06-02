cd65d8f285603b646cb31c3ba9d893999f149f06
Biswajit1729 <mandalbis1729@gmail.com>
2021-06-02 22:53:27 +0530

    3rd commit

--- functions/api.js
+++ functions/api.js
@@ -2,9 +2,9 @@ const express = require('express');
 const TorrentSearchApi = require('torrent-search-api');
 const app = express();
 const port = 3000;
+TorrentSearchApi.enableProvider('1337x');
 TorrentSearchApi.enableProvider('Torrent9');
 TorrentSearchApi.enableProvider('Torrentz2');
-TorrentSearchApi.enableProvider('1337x');
 TorrentSearchApi.enableProvider('ThePirateBay');
 TorrentSearchApi.enableProvider('KickassTorrents');
 TorrentSearchApi.enableProvider('Rarbg');
@@ -16,9 +16,14 @@ TorrentSearchApi.enableProvider('Eztv');
 
 
 app.get('/', async (req, res)  => {
-    const torrents = await TorrentSearchApi.search(
-        ['Torrent9','Torrentz2','1337x','ThePirateBay','KickassTorrents','Rarbg','TorrentProject','Yts','Limetorrents','Eztv'], ['720','hindi'], 'Movies', 20);
-    res.send(torrents)
+    try {
+        const torrents = await TorrentSearchApi.search(
+            ['1337x','Torrentz2','Torrent9','ThePirateBay','KickassTorrents','Rarbg','TorrentProject','Yts','Limetorrents','Eztv'], ['720','hindi'], 'Movies', 50);
+        res.send(torrents)
+    } catch (error) {
+        console.log(error);
+    }
+    
 });
 
 app.listen(process.env.PORT ||  port, () => {
